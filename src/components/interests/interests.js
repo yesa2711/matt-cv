0fd4714177e8282b052acfce50c0e19bb9bf17c4
yesa2711 <yesamokolensang@gmail.com>
2024-11-19 07:49:24 +0800

    adding interests code

---
+++ src/components/interests/interests.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const interests = () => {
+  return (
+    <section className="resume-section" id="interests">
+      <div className="resume-section-content">
+        <h2 className="mb-5">Interests</h2>
+        <p>
+          Apart from being a web developer, I enjoy most of my time being
+          outdoors. In the winter, I am an avid skier and novice ice climber.
+          During the warmer months here in Colorado, I enjoy mountain biking,
+          free climbing, and kayaking.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default interests;
