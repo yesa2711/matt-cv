af5a0e8b49a94a4591115c6027ead253edb9d037
yesa2711 <yesamokolensang@gmail.com>
2024-11-19 07:41:50 +0800

    adding code about

---
+++ src/components/about/about.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <section className="resume-section" id="about">
+      <div className="resume-section-content">
+        <h1 className="mb-0">
+          Clarence <span className="text-primary">Taylor</span>
+        </h1>
+        <div className="subheading mb-5">
+          3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·{" "}
+          <a href="mailto:name@email.com">name@email.com</a>
+        </div>
+        <p className="lead mb-5">
+          I am experienced in leveraging agile frameworks to provide a robust
+          synopsis for high-level overviews.
+        </p>
+        <div className="social-icons">
+          <a className="social-icon" href="#!">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a className="social-icon" href="#!">
+            <i className="fab fa-github"></i>
+          </a>
+          <a className="social-icon" href="#!">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a className="social-icon" href="#!">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
