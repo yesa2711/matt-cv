974924e64754732f227d95b6c939d831d1d625e8
yesa2711 <yesamokolensang@gmail.com>
2024-11-19 22:27:39 +0800

    mengganti isi experience

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -7,18 +7,20 @@ const Experience = () => {
         <h2 className="mb-5">Experience</h2>
         <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
           <div className="flex-grow-1">
-            <h3 className="mb-0">Senior Web Developer</h3>
+            <h3 className="mb-0">Junior Web Designer</h3>
             <div className="subheading mb-3">Intelitec Solutions</div>
             <p>
-              Bring to the table win-win survival strategies to ensure proactive
-              domination. At the end of the day, going forward, a new normal
-              that has evolved from generation X is on the runway heading
-              towards a streamlined cloud solution. User-generated content in
-              real-time will have multiple touchpoints for offshoring.
+              Menerapkan pengetahuan HTML, CSS, dan JavaScript untuk
+              mengembangkan berbagai proyek web responsif sebagai bagian dari
+              tugas dan kerja tim. Membuat desain antarmuka pengguna yang
+              intuitif sambil memastikan kompatibilitas lintas browser.
+              Mengintegrasikan animasi sederhana menggunakan JavaScript untuk
+              meningkatkan pengalaman pengguna dan mengeksplorasi konsep
+              pengembangan web modern melalui proyek-proyek kreatif.
             </p>
           </div>
           <div className="flex-shrink-0">
-            <span className="text-primary">March 2013 - Present</span>
+            <span className="text-primary">November 2024 - Present</span>
           </div>
         </div>
         {/* Tambahkan pengalaman lainnya jika diperlukan */}
